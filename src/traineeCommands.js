f0537ec1f673607dc25e5474805b54ee5586398e
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-02-23 12:02:59 +0100

    Add traineeCommands

--- src/traineeCommands.js
+++ src/traineeCommands.js
@@ -1,18 +1,22 @@
 import chalk from 'chalk';
-import { saveTraineeData, loadTraineeData } from './storage.js';
+import { saveTraineeData, loadTraineeData, loadCourseData } from './storage.js';
 
 function addTrainee(firstName, lastName) {
+  if (!firstName || !lastName) {
+    throw new Error(chalk.red('Must provide first and last name'));
+  }
+
   const id = Math.floor(Math.random() * 100000);
   const newTrainee = { id, firstName, lastName };
 
-  const traineesArray = loadTraineeData();
+  const trainees = loadTraineeData();
 
-  if (traineesArray.some((trainee) => trainee.id === id)) {
+  if (trainees.some((trainee) => trainee.id === id)) {
     throw new Error(chalk.red(`This id already exist`));
   }
 
   if (
-    traineesArray.some(
+    trainees.some(
       (trainee) =>
         trainee.firstName === firstName && trainee.lastName === lastName
     )
@@ -24,38 +28,107 @@ function addTrainee(firstName, lastName) {
     );
   }
 
-  traineesArray.push(newTrainee);
+  trainees.push(newTrainee);
 
-  saveTraineeData(traineesArray);
+  saveTraineeData(trainees);
 
   return `CREATED: ${id} ${firstName} ${lastName}`;
 }
 
-function updateTrainee() {
-  // TODO: Implement the logic
+function getTraineeByIdOrThrow(id) {
+  const trainees = loadTraineeData();
+
+  const trainee = trainees.find((trainee) => trainee.id === id);
+
+  if (!trainee) {
+    throw new Error(chalk.red(`Trainee with id ${id} doesn't exist`));
+  }
+
+  return trainee;
 }
 
-function deleteTrainee() {
-  // TODO: Implement the logic
+function updateTrainee(id, firstName, lastName) {
+  if (!id || !firstName || !lastName) {
+    throw new Error(chalk.red(`Must provide ID, first name and last name`));
+  }
+
+  const trainees = loadTraineeData();
+  const trainee = getTraineeByIdOrThrow();
+
+  trainee.firstName = firstName;
+  trainee.lastName = lastName;
+
+  saveTraineeData(trainees);
+
+  return `UPDATED: ${id} ${firstName} ${lastName}`;
 }
 
-function fetchTrainee() {
-  // TODO: Implement the logic
+function deleteTrainee(id) {
+  const { firstName, lastName } = getTraineeByIdOrThrow(id);
+  const traineesWithoutDeleted = trainees.filter(
+    (trainee) => trainee.id !== id
+  );
+
+  saveTraineeData(traineesWithoutDeleted);
+
+  return `DELETED: ${id} ${firstName} ${lastName}`;
+}
+
+function fetchTrainee(id) {
+  const { firstName, lastName } = getTraineeByIdOrThrow(id);
+  const coursesInfo = loadCourseData();
+
+  const traineeCourses = coursesInfo
+    .filter((course) => course.participants.includes(id))
+    .map((course) => course.name)
+    .join(', ');
+
+  return `${id} ${firstName} ${lastName}\nCourses: ${traineeCourses}`;
 }
 
 function fetchAllTrainees() {
-  // TODO: Implement the logic
+  const trainees = loadTraineeData();
+  trainees.sort((a, b) => a.lastName.localeCompare(b.lastName));
+
+  const traineesInfo = trainees
+    .map(
+      (trainee) =>
+        `${chalk.bold(trainee.id)} ${chalk.cyan(trainee.firstName)} ${chalk.cyanBright(trainee.lastName)}`
+    )
+    .join('\n');
+
+  const summary = `${chalk.bgYellowBright.bold('Trainees:')}\n${traineesInfo}\n\n${chalk.bgYellowBright.bold('Total:')} ${chalk.bold(trainees.length)}`;
+
+  return summary;
 }
 
 export function handleTraineeCommand(subcommand, args) {
-  // Read the subcommand and call the appropriate function with the arguments
+  switch (subcommand) {
+    case 'ADD': {
+      const [firstName, lastName] = args;
+      return addTrainee(firstName, lastName);
+    }
+
+    case 'UPDATE': {
+      const [id, firstName, lastName] = args;
+      return updateTrainee(Number(id), firstName, lastName);
+    }
+
+    case 'DELETE': {
+      const [id] = args;
+      return deleteTrainee(Number(id));
+    }
+
+    case 'GET': {
+      const [id] = args;
+      return fetchTrainee(Number(id));
+    }
+
+    case 'GETALL': {
+      return fetchAllTrainees();
+    }
+
+    default:
+      throw new Error(chalk.red(`Invalid TRAINEE subcommand: ${subcommand}`));
+  }
 }
-
-// const isString =
-//   typeof firstName === 'string' && typeof lastName === 'string';
-
-// if (!isString) {
-//   throw new Error(
-//     chalk.red(`First name and ast name of trainee must be a string`)
-//   );
-// }
